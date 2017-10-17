85f5f006a3a7d35c11780198306140e22de8fb6a
artem_vasenin <rusich.accenture@mail.ru>
2017-10-17 16:45:27 +0300

    Добавлены файлы проекта

---
+++ client/main.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import { Store, createStore } from 'redux';
+// import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+  document.getElementById('app')
+);
